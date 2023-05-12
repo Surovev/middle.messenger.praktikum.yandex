7695db9f098a2d86e02cf4621471c58810d91466
Dmitrii Surovyev <dmitriysurovrv@gmail.com>
2023-05-12 11:32:08 +0300

    add test ability

--- src/test/babel-register.js
+++ src/test/babel-register.js
@@ -1,35 +1,3 @@
-
-//const { JSDOM } = require("jsdom");
 const register = require("@babel/register").default;
 
 register({ extensions: [".ts", ".js"] });
-
-// const dom = new JSDOM('<div id="root"><div>', { url: "http://localhost:3000" });
-// console.log(dom.window.localStorage);
-// global.window = dom.window;
-// global.document = dom.window.document;
-// //global.window.localStorage = dom.window.localStorage;
-// global.window.history = dom.window.History;
-
-// global.HTMLElement = class {
-//   innerHtml = "";
-// };
-
-// global.XMLHttpRequest = class {
-//   status = 200;
-//   onload = () => null;
-
-//   setRequestHeader = (headers) => null;
-
-//   getResponseHeader = (header) => {
-//     return "no headers";
-//   };
-
-//   open = (method, url) => null;
-
-//   send = (data) => {
-//     this.status = 200;
-//     this.response = "OK";
-//     this.onload();
-//   };
-// };
